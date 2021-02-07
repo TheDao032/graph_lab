39c689bd18913e53e7789ef0a6d3390822abc81b
TheDao032 <nthedao2705@gmail.com>
2021-02-07 13:12:59 +0700

    update

--- Lab02-Search/student_functions.py
+++ Lab02-Search/student_functions.py
@@ -9,10 +9,27 @@ def convertToDict(input):
         for j in i:
             if int(j) > 0:
                 newMatrix[flag].append(temp)
-                temp += 1
+                temp = temp + 1
             else:
-                temp += 1
-        flag += 1
+                temp = temp + 1
+        flag = flag + 1
+    return newMatrix
+
+def convertToUscDict(input):
+    newMatrix = {}
+    flag = 0
+    for i in input:
+        newMatrix[flag] = list()
+        temp = 0
+        for j in i:
+            relateNode = {}
+            if int(j) > 0:
+                relateNode[temp] = [int(j), flag]
+                newMatrix[flag].append(relateNode)
+                temp = temp + 1
+            else:
+                temp = temp + 1
+        flag = flag + 1
     return newMatrix
 
 def DFS(matrix, start, end):
@@ -40,19 +57,24 @@ def DFS(matrix, start, end):
    
     path=[]
     visited={}
-    result=[]
-    tempPath=[]
-    tempVisit={}
-    flag = start
-    tempPath.append(flag)
-
     newMatrix = convertToDict(matrix)
-    flag = start
-
-    while (end not in tempPath):
-        result.append(flag)
-        for i in newMatrix[flag]:
+    key = start
+    path.append(key)
+    while key != end:
+        tempPath=[]
+        visited[key] = newMatrix[key]
+        print(visited[key])
+        for i in visited[key]:
             tempPath.append(i)
+        print(tempPath)
+        for i in tempPath:
+            if i in visited.keys():
+                continue
+            key = i
+        path.append(key)
+        print(path)
+    print(path)
+    print(visited)
     
     return visited, path
 
@@ -81,6 +103,21 @@ def BFS(matrix, start, end):
     
     path=[]
     visited={}
+    newMatrix = convertToDict(matrix)
+    key = start
+    path.append(key)
+    while end not in path:
+        tempPath=[]
+        visited[key] = newMatrix[key]
+        for i in visited[key]:
+            tempPath.append(i)
+        if end in tempPath:
+            path.append(end)
+        else:
+            key = tempPath[0]
+            path.append(key)
+    print(path)
+    print(visited)
    
     return visited, path
 
@@ -107,7 +144,45 @@ def UCS(matrix, start, end):
     """
     # TODO: 
     path=[]
-    visited={}
+    # fullPath=[]
+    # visited={}
+    # newMatrix = convertToUscDict(matrix)
+    # print(newMatrix)
+
+    # key = start
+    # fullPath.append(key)
+    # # listCostKey = newMatrix.keys()
+    # minValue = None
+    # tempVisited = {}
+    # while end not in fullPath:
+    #     tempKeyVisited = tempVisited.keys()
+    #     for i in newMatrix[key]:
+    #         costKey = i.keys()
+    #         costKey = list(costKey)
+    #         if costKey[0] not in tempKeyVisited and costKey[0] != start:
+    #             tempVisited[costKey[0]] = i[costKey[0]]
+    #     costKey = tempVisited.keys()
+    #     costKey = list(costKey)
+    #     key = costKey[0]
+    #     for j in tempVisited:
+    #         if minValue == None:
+    #             minValue = tempVisited[costKey[0]][0]
+    #         if tempVisited[j][0] < minValue and j not in path:
+    #             minValue = tempVisited[j][0]
+    #             key = j
+    #     # print(key)
+    #     visited[key] = tempVisited[key]
+    #     tempVisited.pop(key)
+    #     # print(visited)
+    #     fullPath.append(key)
+    # # fullPath.reverse()
+    # path.append(end)
+    # fullKey = end
+    # while start not in path:
+    #     path.append(visited[fullKey][1])
+    #     fullKey = visited[fullKey][1]
+    # path.reverse()
+    # print(visited, path)
     return visited, path
 
 
